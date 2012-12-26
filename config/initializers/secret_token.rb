6cd479273b3478e7ed5991dab0c46e1629e01ef8
joeryan <joe@clanryan.us>
2012-12-27 01:14:07 +0430

    Initial comit of rails sample app from ruby on rails: learn by example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAp::Application.config.secret_token = '0e3ace9b346c364ef8a94740787d9f62e172b2a5cdbeea3f67cea9a3b25daad64806840b192e72644fde61049d0163bf85cdfcfeb28a3562f7802e9d8ff8bddb'
